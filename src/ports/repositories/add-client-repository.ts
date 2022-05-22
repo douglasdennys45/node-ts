be77c6540312b6bf7380a49d901d473e158556ff
Douglas Dennys <douglasdennys45@gmail.com>
2022-05-22 20:10:58 -0300

    ajuste no gitignore e alterando o import do add-client-repository

--- src/ports/repositories/add-client-repository.ts
+++ src/ports/repositories/add-client-repository.ts
@@ -1,12 +1,10 @@
+import { ClientRequest } from '@/entities/types'
+
 export interface AddClientRepository {
   create(payload: AddClientRepository.Request): Promise<AddClientRepository.Response>
 }
 
 export namespace AddClientRepository {
-  export type Request = {
-    name: string
-    document?: Document[]
-    type: string
-  }
+  export type Request = ClientRequest
   export type Response = string
 }
