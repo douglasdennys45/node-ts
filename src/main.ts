d45a680c0f7a3d29aa367bada2829d887fc4cc88
Douglas Dennys <douglasdennys45@gmail.com>
2022-10-19 15:56:33 -0300

    ajustando subida de projeto

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,14 @@ import 'dotenv/config'
 import './infrastructure/config/module-alias'
 
 import env from '@/infrastructure/config/env'
-import { MongoConnection } from '@/infrastructure/database'
+import { MongoConnection } from '@/infrastructure/database/mongodb'
 
 MongoConnection.connect(env.MONGO_URL)
   .then(async () => {
+    console.log('ers')
     const { app } = await import('@/infrastructure/config/app')
     app.listen(env.APP_PORT, () => console.log(`Server running at http://localhost:${env.APP_PORT}`))
+  })
+  .catch((err) => {
+    console.log(`error mongodb: ${err}`)
   })
